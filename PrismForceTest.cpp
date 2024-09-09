390350dd24793b08ac23934bb9060efd308e9e46
Agnisha <2025.cse09.agnishade@gmail.com>
2024-09-10 01:34:55 +0530

    Modified some conditions

--- PrismForceTest.cpp
+++ PrismForceTest.cpp
@@ -7,24 +7,28 @@ int dp(int power, int index, int skip, int recharge, int iniPower, vector<int> &
 	if (index > 10) return 1;
 	
 	// checking if the answer of this sub-problem is already stored in 'mem' or not. If it is stored, simply returning the answer.
-	if(mem[power][index][skip][recharge] != -1)
-	    return mem[power][index][skip][recharge];
+	if (power >= 0 && power < mem.size() && index <= 10 && skip < mem[0][0].size() && recharge < mem[0][0][0].size()) {
+        if(mem[power][index][skip][recharge] != -1)
+            return mem[power][index][skip][recharge];
+    } else {
+        return 0;  
+    }
 
 	int answer = 0;
 	// if Abhimanyu wants to fight current and he is actually worthy enough
-	if (power - enemy[index] >= 0) {
+	if (power - enemy[index] > 0) {
 		// if the back enemy doesn't want to fight because he is scared of Abhimanyu
 		answer |= dp(power - enemy[index], index + 1, skip, recharge, iniPower, enemy, mem);
 
 		if (index == 3 || index == 7) {
 			// if the back enemy wants to fight and Abhimanyu wants to fight him too and he is actually worthy enough
-			if (power - enemy[index] - (enemy[index - 1] >> 1) >= 0) answer |= dp(power - enemy[index] - (enemy[index - 1] >> 1), index + 1, skip, recharge, iniPower, enemy, mem);
+			if (power - enemy[index] - (enemy[index - 1] >> 1) > 0) answer |= dp(power - enemy[index] - (enemy[index - 1] >> 1), index + 1, skip, recharge, iniPower, enemy, mem);
 
 			// if the back enemy wants to fight but Abhimanyu wants to skip him
 			if (skip) answer |= dp(power - enemy[index], index + 1, skip - 1, recharge, iniPower, enemy, mem);
 
 			// if the back enemy wants to fight but Abhimanyu is not worthy enough but will become worthy after a quick recharge
-			if (recharge && (power - enemy[index] - (enemy[index - 1] >> 1) + iniPower) >= 0) answer |= dp(power - enemy[index] - (enemy[index - 1] >> 1) + iniPower, index + 1, skip, recharge - 1, iniPower, enemy, mem);
+			if (recharge && (power - enemy[index] - (enemy[index - 1] >> 1) + iniPower) > 0) answer |= dp(power - enemy[index] - (enemy[index - 1] >> 1) + iniPower, index + 1, skip, recharge - 1, iniPower, enemy, mem);
 		}
 	}
 
@@ -35,30 +39,30 @@ int dp(int power, int index, int skip, int recharge, int iniPower, vector<int> &
 
 		if (index == 3 || index == 7) {
 			// if the back enemy wants to fight and Abhimanyu wants to fight him too beacause he too bored without work
-			if (power - (enemy[index - 1] >> 1) >= 0) answer |= dp(power - (enemy[index - 1] >> 1), index + 1, skip - 1, recharge, iniPower, enemy, mem);
+			if (power - (enemy[index - 1] >> 1) > 0) answer |= dp(power - (enemy[index - 1] >> 1), index + 1, skip - 1, recharge, iniPower, enemy, mem);
 
 			// if the back enemy wants to fight but Abhimanyu wants to skip him. 
 			if (skip > 1) answer |= dp(power, index + 1, skip - 2, recharge, iniPower, enemy, mem);
 
 			// if the back enemy wants to fight but Abhimanyu is not worthy enough but will become worthy after a quick recharge
-			if (recharge && (power - (enemy[index - 1] >> 1) + iniPower) >= 0) answer |= dp(power - (enemy[index - 1] >> 1) + iniPower, index + 1, skip-1, recharge - 1, iniPower, enemy, mem);
+			if (recharge && (power - (enemy[index - 1] >> 1) + iniPower) > 0) answer |= dp(power - (enemy[index - 1] >> 1) + iniPower, index + 1, skip-1, recharge - 1, iniPower, enemy, mem);
 		}
 	}
 
 	// if Abhimanyu is not worthy enough but will become worthy after a quick recharge
-	if (recharge && (power - enemy[index] + iniPower >= 0)) {
+	if (recharge && (power - enemy[index] + iniPower > 0)) {
 		// if the back enemy also doesn't want to fight because he is scared of Abhimanyu
 		answer |= dp(power - enemy[index] + iniPower, index + 1, skip, recharge - 1, iniPower, enemy, mem);
 
 		if (index == 3 || index == 7) {
 			// if the back enemy wants to fight and Abhimanyu wants to fight him too because he too strong after charging
-			if (power - enemy[index] + iniPower - (enemy[index - 1] >> 1) >= 0) answer |= dp(power - enemy[index] + iniPower - (enemy[index - 1] >> 1), index + 1, skip, recharge - 1, iniPower, enemy, mem);
+			if (power - enemy[index] + iniPower - (enemy[index - 1] >> 1) > 0) answer |= dp(power - enemy[index] + iniPower - (enemy[index - 1] >> 1), index + 1, skip, recharge - 1, iniPower, enemy, mem);
 
 			// if the back enemy wants to fight but Abhimanyu wants to skip him.
 			if (skip) answer |= dp(power - enemy[index] + iniPower, index + 1, skip - 1, recharge - 1, iniPower, enemy, mem);
 
 			// if the back enemy wants to fight but Abhimanyu is still not worthy enough but will become worthy after a quick recharge 
-			if (recharge > 1 && (power - enemy[index] - (enemy[index - 1] >> 1) + 2 * iniPower) >= 0) answer |= dp(power - enemy[index] - (enemy[index - 1] >> 1) + 2 * iniPower, index + 1, skip, recharge - 2, iniPower, enemy, mem);
+			if (recharge > 1 && (power - enemy[index] - (enemy[index - 1] >> 1) + 2 * iniPower) > 0) answer |= dp(power - enemy[index] - (enemy[index - 1] >> 1) + 2 * iniPower, index + 1, skip, recharge - 2, iniPower, enemy, mem);
 		}
 	}
 
